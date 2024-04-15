e882a8fc4b474da3f0de4cb4b1280a211b8be1bd
Ammar@SITLPC <ammarajml@gmail.com>
2024-04-16 00:02:31 +0900

    SD card saving done.!

---
+++ lib/NEOPIXEL/NEOPIXEL.cpp
@@ -0,0 +1,30 @@
+#ifndef __NEOPIXEL_CPP__
+#define __NEOPIXEL_CPP__
+
+#include "NEOPIXEL.hpp"
+#define NEOPIXELPIN 8
+NeoPixel_qr::NeoPixel_qr() {
+    // constructor
+}
+NeoPixel_qr::~NeoPixel_qr() {
+    // destructor
+}
+
+// Adafruit_NeoPixel onePixel = Adafruit_NeoPixel(1, NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
+bool NeoPixel_qr::init() {
+    onePixel = Adafruit_NeoPixel(1, NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
+    onePixel.begin();
+    onePixel.clear();
+    onePixel.setPixelColor(0, 200, 0, 0);
+    onePixel.setBrightness(10);
+    onePixel.show();
+    return true;
+}
+void NeoPixel_qr::setPixelColor(int n, int r, int g, int b) {
+    onePixel.clear();
+    onePixel.setPixelColor(n, r, g, b);
+    onePixel.show();
+    delay(400);
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,97 @@
 #include <Adafruit_LittleFS.h>
 #include <InternalFileSystem.h>
 #include <Adafruit_SleepyDog.h>
+#include "NEOPIXEL.hpp"
+#include "epd4in2_V2.h"
+#include "epdpaint.h"
+using namespace Adafruit_LittleFS_Namespace;
+//SENSOR ID
+#define NODEID "N02 "
 
-#define ADXL355_CS_PIN    18
-#define ADXL355_DRDY_PIN  17
-#define ADXL362_CS_PIN    16
-#define ADXL355_ADDRESS   0x1D
+//ADC
+
+#define PGA 32                // Programmable Gain, confirm that the same as set_pga_gain
+#define VREF 2.048            // Internal reference of 2.048V
+#define VFSR VREF/PGA
+#define FSR (((long int)1<<23)-1)
 #define ADS1220_CS_PIN    19
 #define ADS1220_DRDY_PIN  5
-#define THERMISTOR_PIN    A1
-#define REFERENCE_RESISTANCE  10000
-#define NOMINAL_RESISTANCE    10000
-#define NOMINAL_TEMPERATURE   25
-#define B_VALUE   3950
-#define NUM_SAMPLES 100
 
+#define COLORED      0
+#define UNCOLORED    1
 
-// Define the pin number for the LED
+//DEFAULT
+#define ST_WAIT 1500        // ms
+#define DATA_RATE 1         // ksps, internal
+#define TIM_Waitms(...)   delay(__VA_ARGS__)
+#define SUCCESS true
+#define ERROR false
 #define LED_PIN 4
 
+//EPD
+#define QRversion    17
+
+//RTC
+bool Century = false;
+byte year1, month1, date1, hour1, minute1, second1;
+
+//POWER
+#define LATCH 2 //LATCH
+//ADXL
+#define ADXL355_CS_PIN    18
+#define ADXL355_DRDY_PIN    17
+#define ADXL362_CS_PIN    16
+#define ADXL355_ADDRESS 0x1D
+bool ADXL355_DRDY=0;
+volatile int ADXL355_DATA_COUNTER=0;
+ADXL362 adxl362;
+ADXL355 adxl355(ADXL355_CS_PIN);
+int NUM_SAMPLES=100; //1 min 60s * 125Hz
+//SD
+//#define DEV  SD
+int linesCount = 0; //LFS saved Line Count
+#define SDCARD_SS_PIN 10
+//LFS FILE NAME
+#define FLAG_PATH "init6.txt"
+#define DATA_PATH "DATATEMP6.txt"
+#define SD_PATH "NODE02.txt"
+
+// resistance values and parameters for the thermistor
+#define THERMISTOR_PIN A1 // Analog pin connected to the thermistor
+#define REFERENCE_RESISTANCE 10000
+#define NOMINAL_RESISTANCE 10000
+#define NOMINAL_TEMPERATURE 25
+#define B_VALUE 3950
+
+//EDS setting
+int adxl_thresh = 120;
+uint8_t NumOfEvent = 1;
+uint8_t DayOfEvent = 2;
+int SensorID = 21;
+//DS3231 Clock;
+RTC_DS3231 rtc;
+
+//ADC
+
+ADS1220_WE ads1220 = ADS1220_WE(ADS1220_CS_PIN, ADS1220_DRDY_PIN);
+
 volatile bool drdyIntrFlag = false;
+
+
+//ADXL362
+extern void adxl362_init(int adxl_act_thresh, int adxl_act_interval);
+//LED
+NeoPixel_qr nRFBoard_neopixel;
+
+SDLib::File myFile;
+Adafruit_LittleFS_Namespace::File file(InternalFS);
+Adafruit_LittleFS lfs;
+//EPD
+Epd epd;
+//SAVING
+char resultString[200]; // Adjust the size based on the expected length of the message
+
+
 
 class RTCManager {
 private:
@@ -41,18 +113,36 @@ private:
 
 public:
     RTCManager() {}
+    char timestampBuffer[13];
 
     void initialize() {
+        Serial.print("Initializing RTC... ");
         if (!rtc.begin()) {
             Serial.println("Couldn't find RTC");
             Serial.flush();
-            while (1);
+            while (1) delay(10);
         }
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        Serial.println("RTC Initialized Successfully..!");
+        // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
 
-    DateTime getCurrentDateTime() {
-        return rtc.now();
+    
+
+    const char* getCurrentTimestamp() {
+        DateTime now = rtc.now();
+
+        // Format the timestamp and store it in the timestampBuffer array
+        snprintf(timestampBuffer, sizeof(timestampBuffer), "%02d%02d%02d%02d%02d%02d",
+                 now.year()%100, now.month(), now.day(), now.hour(), now.minute(), now.second());
+
+        return timestampBuffer; // Return the pointer to the timestampBuffer array
+    }
+    void clearAlarmRTC(){
+        rtc.clearAlarm(1);
+        rtc.clearAlarm(2);
+        rtc.writeSqwPinMode(DS3231_OFF);
+        rtc.disable32K();
+        rtc.disableAlarm(2);
     }
 };
 
@@ -61,28 +151,77 @@ public:
     SDFileManager() {}
 
     void initialize() {
+        Serial.print("Initializing SD Card... ");
         if (!SD.begin(10)) {
             Serial.println("SD initialization failed!");
             while (1);
         }
+        else {
+            Serial.println("SD Initialized Successfully..!");
+        }
     }
 
     void writeData(const char* filename, const char* data) {
-        File file = SD.open(filename, FILE_WRITE);
-        if (file) {
-            file.println(data);
-            file.close();
-            Serial.println("Data written to SD card.");
-        } else {
-            Serial.println("Error opening file for writing.");
+        
+        Serial.println("Writing data to SD card");
+        if (!SD.exists(filename)) {
+            Serial.println("File does not exist. Creating file...");
+            myFile = SD.open(filename, FILE_WRITE);
+            myFile.close();
+        }
+        else {
+            Serial.println("File exists. Opening file for data writing...");
+            myFile = SD.open(filename, FILE_WRITE);
+            myFile.println(data);
+            myFile.close();
+            Serial.println("Data written to SD card successfully.");
+        }
+    }
+    void readData(const char* filename) {
+        if (SD.exists(filename)) {
+            Serial.println("Reading data from SD card");
+            myFile = SD.open(filename, FILE_READ);
+            while (myFile.available()) {
+                Serial.write(myFile.read());
+            }
+            myFile.close();
+            Serial.println("Data read from SD card successfully.");
+        }
+        else {
+            Serial.println("File does not exist. Cannot read data.");
         }
     }
 };
 
+class LittleFSManager {
+public:
+    LittleFSManager() {}
+
+    void initialize() {
+        Serial.print("Initializing LITTLEFS... ");
+        if (!InternalFS.begin()) {
+            Serial.println("LittleFS initialization failed!");
+            while (1);
+        }
+        Serial.println("LITTLEFS Initialized Successfully..!");
+    }
+
+    // void writeData(const char* filename, const char* data) {
+    //     File file = InternalFS.open(filename, FILE_WRITE);
+    //     if (file) {
+    //         file.println(data);
+    //         file.close();
+    //         Serial.println("Data written to LittleFS.");
+    //     } else {
+    //         Serial.println("Error opening file for writing.");
+    //     }
+    // }
+};
+
 class SensorManager {
   private:
-    bool ADXL355_DRDY;
-    volatile int ADXL355_DATA_COUNTER;
+    static bool ADXL355_DRDY;
+    static volatile int ADXL355_DATA_COUNTER;
     ADXL355 adxl355;
     ADS1220_WE ads1220;
 
@@ -93,12 +232,37 @@ public:
         ADXL355_DATA_COUNTER = 0;
     }
 
+    static void handleDRDYInterrupt355() {
+        // Serial.println("Setting up DRDY Interrupt 355");
+        ADXL355_DRDY = true;
+        ADXL355_DATA_COUNTER += 1;
+        // Serial.print("ADXL355_DRDY: ");
+        // Serial.println(ADXL355_DRDY);
+        // Serial.print("ADXL355_DATA_COUNTER: ");
+        // Serial.println(ADXL355_DATA_COUNTER);
+        // Serial.println("DRDY Interrupt 355 set up successfully..!");
+    }
+    static void handleDRDYInterrupt1220() {
+        // Serial.println("Setting up DRDY Interrupt 1220");
+        drdyIntrFlag = true;
+        // Serial.print("DRDY Interrupt 1220: ");
+        // Serial.println(drdyIntrFlag);
+        // Serial.println("DRDY Interrupt 1220 set up successfully..!");
+    }
+
     void initialize() {
+        Serial.print("Initializing SENSORS... ");
         adxl355.begin();
+
+        
         ads1220.init();
         ads1220.setDataRate(ADS1220_DR_LVL_0);
         ads1220.setConversionMode(ADS1220_CONTINUOUS);
-        enableInterruptPin();
+        // attachInterrupt(digitalPinToInterrupt(ADXL355_DRDY_PIN), handleDRDYInterrupt, RISING);
+        // Serial.println("now attaching the interrupts..!");
+        attachInterrupt(digitalPinToInterrupt(ADXL355_DRDY_PIN),handleDRDYInterrupt355, RISING);
+        attachInterrupt(digitalPinToInterrupt(ADS1220_DRDY_PIN), handleDRDYInterrupt1220, FALLING);
+        Serial.println("SENSORS Initialized Successfully..!");
     }
 
     float readTemperature() {
@@ -115,7 +279,7 @@ public:
         return T - 273.15;
     }
 
-    void readAcceleration(float& x, float& y, float& z) {
+    void readAcceleration(float& xMean, float& yMean, float& zMean, float &tiltX, float &tiltY) {
         float xSamples[NUM_SAMPLES], ySamples[NUM_SAMPLES], zSamples[NUM_SAMPLES];
         accSensing(xSamples, ySamples, zSamples, NUM_SAMPLES);
 
@@ -126,9 +290,13 @@ public:
             zSum += zSamples[i];
         }
 
-        x = xSum / NUM_SAMPLES;
-        y = ySum / NUM_SAMPLES;
-        z = zSum / NUM_SAMPLES;
+        xMean = xSum / NUM_SAMPLES;
+        yMean = ySum / NUM_SAMPLES;
+        zMean = zSum / NUM_SAMPLES;
+
+        tiltX = atan2(xMean, sqrt(yMean * yMean + zMean * zMean)) * (180.0 / PI);
+        tiltY = atan2(yMean, sqrt(xMean * xMean + zMean * zMean)) * (180.0 / PI);
+
         }
 
     void readStrain(float& strain) {
@@ -155,7 +323,8 @@ public:
     }
 
 
-    void enableInterruptPin() {attachInterrupt(digitalPinToInterrupt(ADS1220_DRDY_PIN), [](){
+    void enableInterruptPin() {
+        attachInterrupt(digitalPinToInterrupt(ADS1220_DRDY_PIN), [](){
     // Your interrupt handling code here
     drdyIntrFlag = true;
     }, FALLING);
@@ -179,6 +348,14 @@ public:
             xSamples[i] = x;
             ySamples[i] = y;
             zSamples[i] = z;
+            
+            // Serial.print("X: ");
+            // Serial.print(x, 6);
+            // Serial.print(" G, Y: ");
+            // Serial.print(y, 6);
+            // Serial.print(" G, Z: ");
+            // Serial.print(z, 6);
+            // Serial.println(" G");
 
             ADXL355_DRDY = false;
             digitalWrite(LED_PIN, LOW);
@@ -203,24 +380,45 @@ public:
         }
     }
 };
-
+bool SensorManager::ADXL355_DRDY = false;
+volatile int SensorManager::ADXL355_DATA_COUNTER = 0;
 // Define classes for LED control, EPD operations, and QR code generation similarly...
 
 class LEDController {
 public:
     LEDController() {}
 
-    void toggleLED(int n) {
-        // Toggle LED logic here
+    void toggleLED(int n, int delayTime = 1000) {
+        // Toggle LED logic
+        for (int i = 0; i < n; i++) {
+            digitalWrite(LED_PIN, HIGH);
+            delay(delayTime/2);
+            digitalWrite(LED_PIN, LOW);
+            delay(delayTime/2);
+        }
     }
 };
 
 class EPDManager {
+private:
+    unsigned char image[((EPD_WIDTH * EPD_HEIGHT) / 8)] = {0};
+    Paint paint = Paint(image, EPD_WIDTH, EPD_HEIGHT);
+    // Paint paint(image, EPD_WIDTH, EPD_HEIGHT);
+
 public:
     EPDManager() {}
 
     void initialize() {
-        // EPD initialization logic here
+        paint.Clear(UNCOLORED);
+        Serial.print("Initializing EPD... ");
+        if (epd.Init() != 0) {
+            Serial.println("Failed to initialize EPD.");
+            return;
+        }
+        epd.Clear();
+        Serial.println("EPD Initialized Successfully..!");
+
+        
     }
 
     void displayQRCode(const char* qrText) {
@@ -234,49 +432,89 @@ public:
 class SmartSensingSystem {
 private:
     RTCManager rtcManager;
-    SDFileManager sdFileManager;
-    SensorManager sensorManager;
-    LEDController ledController;
     EPDManager epdManager;
+    SensorManager sensorManager;
+    SDFileManager sdFileManager;
+    LittleFSManager littleFSManager;
+    LEDController ledController;
+
+    char dataString[32];
 
 public:
     SmartSensingSystem() {}
 
     void initialize() {
+        // Initialize components
+        pinMode(LED_PIN, OUTPUT);
+        digitalWrite(LED_PIN, HIGH);
         rtcManager.initialize();
+        // epdManager.initialize();
         sdFileManager.initialize();
+        littleFSManager.initialize();
         sensorManager.initialize();
-        // Initialize other components...
+        digitalWrite(LED_PIN, LOW);
     }
 
     void run() {
-        // Main application logic here
-    }
+        /*
+        1. Initialze the littleFS and get the file ready for data saving.
+        2. Get the acceleration data from the ADXL355 sensor, in the form of tiltX, and tiltY.
+        3. Get the temperature data from the thermistor.
+        4. Get the time data from the RTC.
+        5. Construct the data string in the format: "TIMESTAMP, TILT_X, TILT_Y, TEMPERATURE".
+        6. Save the data to the SD card.
+        
+
+        */
+
+        //    2. Getting the acceleration data
+        float xMean = 0, yMean = 0, zMean = 0, tiltX = 0, tiltY = 0;
+        sensorManager.readAcceleration(xMean, yMean, zMean, tiltX, tiltY);
+        Serial.print("X Mean: "); Serial.print(xMean, 6);
+        Serial.print(" G, Y Mean: "); Serial.print(yMean, 6);
+        Serial.print(" G, Z Mean: "); Serial.print(zMean, 6); Serial.println(" G");
+        Serial.print("Tilt X: "); Serial.print(tiltX, 6); Serial.print(" degrees, Tilt Y: "); Serial.print(tiltY, 6); Serial.println(" degrees");
+
+        //    3. Getting the temperature data
+        float temperature = sensorManager.readTemperature();
+        Serial.print("Temperature: "); Serial.print(temperature, 6); Serial.println(" °C");
+
+        //    4. Getting the time data
+        const char* formatedTimeStamp = rtcManager.getCurrentTimestamp();
+        Serial.print("Current Date & Time: "); Serial.println(formatedTimeStamp);
+        Serial.println("--------------------------------------------------");
+
+        //   5. Constructing the data string
+        int j = snprintf(dataString, sizeof(dataString),"%s,%.2f,%.2f,%.1f/", formatedTimeStamp, tiltX, tiltY, temperature);
+        Serial.println(dataString);
+        // Serial.print("Data String Length: "); Serial.print(j);  Serial.println(" bytes");
+        Serial.println("--------------------------------------------------");
+
+        //    6. Saving the data to the SD card and LittleFS
+        sdFileManager.writeData(SD_PATH, dataString);
+        sdFileManager.readData(SD_PATH);
+
+        }
 };
 
 // Create an instance of the main application class
-SmartSensingSystem smartSensingSystem;
+SmartSensingSystem qr_sensor_node2;
 
 // RTCManager time_qr;
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  while (!Serial) delay(10);
-  Serial.print("Hello Ammar, I am still Alive. ");
-  // Serial.println(time_qr.().month(), DEC);
-  // Serial.println("Hello World!");
+    Serial.begin(115200);
+    while (!Serial) delay(10);
+    Serial.println("-----------------------------");
+    Serial.println("STARTING QR SENSOR NODE 2...!");
+    Serial.println("-----------------------------");
+    Serial.println("Setup()");
+    qr_sensor_node2.initialize();
 
-  // watchDogTimerSetup();
 }
 
 
 void loop() {
-    // Reset the watchdog timer
-    // Watchdog.reset();
-    // Serial.println("Working in the loop() !");
-    delay(1000);
-    // The watchdog timer will reset the device if it is not reset within 8 seconds
-    // The device will reset in 8 seconds if the watchdog timer is not reset
-
+    qr_sensor_node2.run();
+    delay(10000);
 }
 
